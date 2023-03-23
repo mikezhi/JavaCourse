0c5b14ab2318ab4f11dd98fdfc52dd2e4c2ee71c
mikezhi <128652927+mikezhi@users.noreply.github.com>
2023-03-23 05:05:42 -0400

    Решение первой задачи

---
+++ src/ru/croc/task1/Main.java
@@ -0,0 +1,10 @@
+package ru.croc.task1;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Решение первой задачи");
+    }
+}
