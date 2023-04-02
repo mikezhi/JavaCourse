bac8a5a542f7d8b2ac1bf9ba286bc189cb4082fd
mikezhi <128652927+mikezhi@users.noreply.github.com>
2023-04-02 13:26:02 -0400

    modify task7

--- src/ru/croc/task7/Main.java
+++ src/ru/croc/task7/Main.java
@@ -1,7 +1,23 @@
 package ru.croc.task7;
 
 public class Main {
+    String source = "/*\n" +
+                    " * My first ever program in JAVA!\n" +
+                    " *\n" +
+                    "class Hello {// class body starts here \n" +
+                    "\n" +
+                    "   /* main method */ \n" +
+                    "   public static void main(String[] args/* we put command line arguments here*/) {\n" +
+                    "      //this line prints my first greeting to the screen \n" +
+                    "      System.out.println(\"Hi\"); // :)\n" +
+                    "   }\n" +
+                    "} // the end\n" +
+                    "// to be continued...\n";
+            ;
     public static void main(String[] args) {
-        System.out.println("Решение сеьмой задачи");
+        String source ="..."; //test data
+        String noComents = removeJavaComments(source);
+        System.out.println(noComents);
     }
+
 }
