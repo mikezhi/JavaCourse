342373987079c7e216e7c20de415fb38de0bed21
mikezhi <128652927+mikezhi@users.noreply.github.com>
2023-04-25 14:30:04 -0400

    add interface for task13

---
+++ src/ru/croc/task13/BlackListFilter.java
@@ -0,0 +1,42 @@
+package ru.croc.task13;
+
+/**
+ * Небольшой стартап разрабатывает социальный сервис, в котором пользова-
+ * тели могут оставлять комментарии. Со временем в комментариях появился спам
+ *
+ * и разработчики решили бороться с ним с помощью "черных списков" запрещенных
+ * слов. Они составили такие списки и поручили вам написать функцию, удаляющую
+ * из всех накопленных комментариев нежелательные.
+ * Они предоставили вам интерфейс, который внедрили в свой продукт, и
+ * попросили написать его реализацию:
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+public interface BlackListFilter<T> {
+    /**
+     * From the given list of comments removes ones
+     * that contain words from the black list.
+     *
+     * @param comments list of comments; every comment
+     * is a sequence of words, separated
+     * by spaces, punctuation or line breaks
+     * @param blackList list of words that should not
+     * be present in a comment
+     */
+
+    default ArrayList<T> filterComments(Iterable<T> comments, Predicate<T> blacklist) {
+        ArrayList<T> filteredResult = new ArrayList<>();
+        Iterator<T> iterator  = comments.iterator();
+
+        while (iterator.hasNext()){
+         T comment  = iterator.next();
+         if (!blacklist.test(comment))
+             filteredResult.add(comment);
+        }
+        return filteredResult;
+    }
+}
--- src/ru/croc/task13/Main.java
+++ src/ru/croc/task13/Main.java
@@ -1,4 +1,11 @@
 package ru.croc.task13;
+import ru.croc.task13.BlackListFilter;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+class Main {
+    public static void main(String[] args) {
+
+    }
 }
