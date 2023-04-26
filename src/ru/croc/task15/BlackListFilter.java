510e8fbf92dc040a641f58ef69c18fb5eb6a895e
mikezhi <128652927+mikezhi@users.noreply.github.com>
2023-04-25 22:44:05 -0400

    add for task15

--- src/ru/croc/task15/BlackListFilter.java
+++ src/ru/croc/task15/BlackListFilter.java
@@ -6,15 +6,15 @@ import java.util.Iterator;
 
 public interface BlackListFilter<T> {
     default ArrayList<T> filterComments(Iterable<T> comments, Predicate<T> blackList) {
-            ArrayList<T> filtrationResult = new ArrayList<>();
-            Iterator<T> it = comments.iterator();
+
+        ArrayList<T> filtrationResult = new ArrayList<>();
+        Iterator<T> it = comments.iterator();
 
         while (it.hasNext()) {
             T comment = it.next();
             if (!blackList.test(comment))
                 filtrationResult.add(comment);
         }
-
         return filtrationResult;
     }
 }
