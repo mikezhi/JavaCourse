ecdbaec07228e9d2202b0efa6c6cad9e688be7fd
mikezhi <128652927+mikezhi@users.noreply.github.com>
2023-04-25 20:45:21 -0400

    refactor class for task13

--- src/ru/croc/task13/Filter.java
+++ src/ru/croc/task13/Filter.java
@@ -6,11 +6,11 @@ import java.util.Set;
 
 public class Filter implements BlackListFilter{
 
-    public void filterComments(List<String> comments, Set<String> blacklist) {
+    public void filterComments(List<String> comments, Set<String> blackList) {
         Iterator<String> iterator = comments.iterator();
         while (iterator.hasNext()){
             String comment = iterator.next();
-            for (String badWord : blacklist){
+            for (String badWord : blackList){
                 if (comment.toUpperCase().contains(badWord))
                     iterator.remove();
             }
