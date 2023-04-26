e4ef1e3a4e313af903870c99c1a62b5ee4b8b4ae
mikezhi <128652927+mikezhi@users.noreply.github.com>
2023-04-25 21:06:36 -0400

    initial for task15

---
+++ src/ru/croc/task15/Main.java
@@ -0,0 +1,9 @@
+package ru.croc.task15;
+
+import java.util.SortedMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Solution for task 15");
+    }
+}
