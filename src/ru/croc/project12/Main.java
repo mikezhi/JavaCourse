948d31d26e50ecae93ab41fbf71f2a8fec365da2
mikezhi <128652927+mikezhi@users.noreply.github.com>
2023-05-01 14:02:09 -0400

    delete final 12

--- src/ru/croc/project12/Main.java
+++ src/ru/croc/project12/Main.java
@@ -2,6 +2,11 @@ package ru.croc.project12;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My project v.12");
+        System.out.println("Solution for project v12");
+
+
+
+
+
     }
 }
