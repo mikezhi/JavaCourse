d531ee67fbf4fa8c8931ec97e8917f40f36835b0
mikezhi <128652927+mikezhi@users.noreply.github.com>
2023-05-03 16:25:29 -0400

    modying project 12

--- src/ru/croc/project12/RoleChecker.java
+++ src/ru/croc/project12/RoleChecker.java
@@ -1,10 +1,8 @@
 package ru.croc.project12;
 
 public class  RoleChecker {
-
     public RoleChecker() {
     }
-
     public boolean hasRole(String role) {
       if (new WordCRUD().roleCheckDB(role)){
           return true;
